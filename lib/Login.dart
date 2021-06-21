706cbbd140a13db62d7c88d0f9fd4ff6d9e7bc2f
patel4 <pr302367@dal.ca>
2021-06-21 07:23:14 -0300

    Designed Login Page UI

---
+++ lib/Login.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+// Define a LoginForm Widget.
+class LoginForm extends StatefulWidget {
+  @override
+  LoginFormState createState() {
+    return LoginFormState();
+  }
+}
+
+// Define a State Class for the Widget.
+class LoginFormState extends State<LoginForm> {
+  // UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(labelText: ' Email'),
+            onChanged: (username) => {print('Username: $username')},
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: ' Password',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () => print("Login Button Pressed"),
+              child: Text('Login'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
