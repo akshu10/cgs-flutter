fce11861fba2d8e4415c1deec4b62acd899420f6
patel4 <pr302367@dal.ca>
2021-06-21 08:00:53 -0300

    Routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'Login.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp(), routes: <String, WidgetBuilder>{
-    'login': (BuildContext context) => LoginForm(),
-  }));
+  runApp(MaterialApp(home: MyApp(),
+      // Routes
+      routes: <String, WidgetBuilder>{
+        'login': (BuildContext context) => LoginForm(),
+      }));
 }
 
 class MyApp extends StatelessWidget {
